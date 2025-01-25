036759a8476867b63f6cf83262e57eae2c47d806
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2025-01-25 08:55:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl">Assignment for SpaceZee company</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
